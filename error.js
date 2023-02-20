b03601283179151facecefe748a8627a2c05ba48
nagibhasan <nagibhasan132@gmail.com>
2023-02-21 00:59:16 +0600

    last commit

---
+++ error.js
@@ -0,0 +1,23 @@
+//not in JS that frequently: range error
+const numbers = [3, 4, 5, 8];
+//you should not do it most of the time
+numbers.length = 2;
+console.log(numbers);
+console.log(numbers.length);
+// console.log(number[25]);
+
+//refarance error
+// console.log(money);
+const money = 80;
+
+// for(let i = 0; i < 5 i++){
+    
+// }
+
+// if(numbers.length > 2){
+
+// }
+// else
+
+let student = {address: 'kochukhet'};
+console.log(student.address.city);
---
+++ recap.js
@@ -0,0 +1,26 @@
+/*
+1.how to use js with html and css
+2.get element or element from dom
+    2-1:getElementById
+    2-2:getElementByClassName
+    2-3:quarySelector
+    2-4:quarySelectorAll
+
+3.dynamically add element to the dom: document.createElement
+    3.5: set value to the innerHTML
+
+4. get nad set value from/to a dom element
+    4-1: use innerText (h1, p, h5, div, spam, strong)
+    4-2: use value (input, textArea)
+
+5. add event lister (event handler)
+    5-1: Click, focus, blur, onChange, keyUp
+    5-2: addEventListener onClick-"myHandler()"
+
+6. use funtion to reduce duplicate code
+
+7.event bobble event delegate
+*/
+
+document.querySelectorAll('.users-container .disable h3');
+document.querySelectorAll('.my-users li')
